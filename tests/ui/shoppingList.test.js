bd87f9a5688d6cea34c031dc3ab366ddc2cb5098
TsvetomirBahov <tsvetomir.bahov@gmail.com>
2023-12-16 15:14:12 +0200

    Fix URL link in ShoppingList UI test

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('https://tsvetomirbahov-exam.onrender.com/');
+    await page.goto('https://tsvetomirbahov-exam.onrender.com/Shopping-List');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
