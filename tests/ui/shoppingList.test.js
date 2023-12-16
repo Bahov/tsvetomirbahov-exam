64472f11f47705fb44dfb713751eab83517c6ad6
TsvetomirBahov <tsvetomir.bahov@gmail.com>
2023-12-16 15:08:38 +0200

    Change URL link in UI tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Shopping-List');
+    await page.goto('https://tsvetomirbahov-exam.onrender.com/');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
